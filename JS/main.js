3be80aa6a479858654d7d4f12bd61cf4763014d6
varun singh <varun8884@gmail.com>
2022-10-14 11:21:12 +0530

    testimonial

---
+++ JS/main.js
@@ -0,0 +1,31 @@
+let swiperPortfolio = new Swiper(".portfolio__container", {
+    cssMode: true,
+    loop: true,
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+  });
+
+
+
+let swiperTestimonial = new Swiper(".testimonial__container", {
+    loop: true,
+    grabCursor:true,
+    spaceBetween: 48,
+
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+      dynamicBullets: true,
+    },
+    breakpoints:{
+        568:{
+            slidesPerView: 2,
+        }
+    }
+  });
